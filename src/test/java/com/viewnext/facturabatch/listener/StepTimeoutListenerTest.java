0354ba0b080e89f79428f6a13d1908e9c2a4ad21
X30253GA <david.galvezcaparros@colaborador.elcorteingles.es>
2026-07-13 12:37:35 +0200

    aplicativo de facturas en procesos batch

--- src/test/java/com/viewnext/facturabatch/listener/StepTimeoutListenerTest.java
+++ src/test/java/com/viewnext/facturabatch/listener/StepTimeoutListenerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -27,6 +29,7 @@ import static org.mockito.Mockito.*;
  * </ul>
  */
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class StepTimeoutListenerTest {
 
     @Mock
